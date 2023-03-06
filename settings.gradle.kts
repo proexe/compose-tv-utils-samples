833a80bd3d69c2ebd3ac12621b90465d6536ebad
Jan Starczewski <jan.a.starczewski@gmail.com>
2023-03-06 19:53:42 +0100

    Init project files

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+enableFeaturePreview("VERSION_CATALOGS")
+
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+rootProject.name = "Compose TV Utils"
+include(":app")
